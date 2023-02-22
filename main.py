d2656efe44694534a5ddd3d977ac22f9daffe55f
roflacock <87777939+Blazef1@users.noreply.github.com>
2023-02-22 11:44:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,18 +8,26 @@
 #print((a+b)*(a**2-a*b+b**2))
 #print((a-b)*(a**2-a*b+b**2))
 
-a = 2
-b = 3
-c = 5
-y1 = a*a
-y2 = a+c
-y3 = y2**2
-y4 = y1+y3
-y5 = 10*b
-y6 = y4/y5
-y7 = c**2
-y8 = a*y7
-y9 = 5*y8
-y10 = y6-y9
-y11= y10**-1/2
-print(y11)
+
+#Второе задание на 4
+#a = 2
+#b = 3
+#c = 5
+#y1 = a*a
+#y2 = a+c
+#y3 = y2**2
+#y4 = y1+y3
+#y5 = 10*b
+#y6 = y4/y5
+#y7 = c**2
+#y8 = a*y7
+#y9 = 5*y8
+#y10 = y6-y9
+#y11= y10**-1/2
+#print(y11)
+
+numbers = 0.1
+while numbers <= 0.3:
+    numbers += 0.1
+numbers = round(numbers,1)
+print(numbers)
